a17ae4065862926ac680d8ffde0522fe4c19a263
veeresh <sodeveeresh2000@gmail.com>
2023-03-13 22:15:48 +0530

    Added string_methods.py

---
+++ string_methods.py
@@ -0,0 +1,18 @@
+
+
+first_name = "Avinash"
+
+last_name = "Veeresh"
+
+full_name1= "Full name is : " + first_name + last_name # string concatenation
+
+full_name2= "Full name is : {0} {1} ".format(first_name,last_name)  # string formatting
+
+full_name3= f"Full name is : {first_name}{last_name} "
+
+
+print(full_name1)
+
+print(full_name2)
+
+print(full_name3)
